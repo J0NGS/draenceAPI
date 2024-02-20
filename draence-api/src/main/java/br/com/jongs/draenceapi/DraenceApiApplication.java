6322c87c805e7dc6b8acbd55623b2436ad88d195
joao <jnetogoncalo@gmail.com>
2024-02-20 09:47:02 -0300

    att

---
+++ draence-api/src/main/java/br/com/jongs/draenceapi/DraenceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jongs.draenceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraenceApiApplication.class, args);
+	}
+
+}
---
+++ draence-api/src/test/java/br/com/jongs/draenceapi/DraenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jongs.draenceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
